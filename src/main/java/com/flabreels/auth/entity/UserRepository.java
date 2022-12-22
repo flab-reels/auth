48a4547f52bd3a76d65d6a27bd37cd5de3b0f9a1
Jongchan Ko <godol811@naver.com>
2022-12-22 23:06:56 +0900

    feature(ID AutoGenerated 수정)
    1. 단순 AI ID를 UUID로 수정
    2. 그에 따른 email값에서 ID 기반으로 조회 저장 가능하게 만듦

--- src/main/java/com/flabreels/auth/entity/UserRepository.java
+++ src/main/java/com/flabreels/auth/entity/UserRepository.java
@@ -3,8 +3,12 @@ package com.flabreels.auth.entity;
 import com.flabreels.auth.dto.UserDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     User findUserByRefreshToken(String refreshToken);
+
     User findUserByEmail(String email);
+
     User findUserById(String id);
 }
--- src/main/java/com/flabreels/auth/oauth/OAuth2SuccessHandler.java
+++ src/main/java/com/flabreels/auth/oauth/OAuth2SuccessHandler.java
@@ -38,7 +38,7 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
         UserDto userDto = userRequestMapper.toDto(oAuth2User);
         TokenResponseDto tokenResponseDto = null;
         //이메일 있으면 중복 체크해서 DB에 쌓이지 않도록 설정
-        User existedUser = userRepository.findUserById(userDto.getEmail());
+        User existedUser = userRepository.findUserByEmail(userDto.getEmail());
         if (existedUser == null){
             tokenResponseDto = tokenService.generateToken(userDto.getId(), userDto.getEmail(), userDto.getPicture() ,Role.USER, Platform.WEB);
             log.info("{}", tokenResponseDto);
